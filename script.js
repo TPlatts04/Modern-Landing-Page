988a9cdf004ac4b21ecb33e71f98300883da0892
Thomas Platts <thomasplatts2004@gmail.com>
2024-01-21 19:51:13 +0000

    Created basic load animation and linked buttons to new html files

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
+// Create hamburger menu when screen is less than 776px or else return to original values
 const listData = []
 for (var i=0; i<=4; i++){
   listData.push($(`.li-${i}`).text())
 }
-
-console.log(listData)
 
 if ($(window).width() < 776){
   $('.unordered').html('<i class="fa-solid fa-bars mt-2 dropdown"></i>').css("paddingLeft", "0px")
